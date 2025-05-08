a5cb00517197995e2dc3aec88cb07031fcc0e1b3
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2025-05-08 18:27:24 +0330

    loading

---
+++ javascript/session12/script.js
@@ -0,0 +1,9 @@
+// function loadHandler(){
+//     console.log("loaded")
+// }
+
+// window.addEventListener('load', loadHandler)
+
+// window.addEventListener('DOMContentLoaded', function(){
+//     console.log("blalalal")
+// })
