24679e2da0826b84bc1b8a2debdc5ff97c8bbfc6
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2025-08-01 14:18:54 +0330

    comm

--- javascript/session19/js/app.js
+++ javascript/session19/js/app.js
@@ -1,2 +1,16 @@
-// DOM => Document Object Model
-// BOM => Browser Object Model
+let productsContainer = document.querySelector('.container')
+let productsArray =[
+    {id:1, title:'Blue Shoe', price:100,img:'images/1.png'},
+    {id:2, title:'Green Shoe', price:90,img:'images/2.png'}
+]
+
+productsArray.forEach(function(product){
+    productsContainer.insertAdjacentHTML('beforeend',
+        '<div class="product-card"><h1>' + product.title + 
+        '</h1><p>Lorem ipsum, or lipsum as it is sometimes known</p>'+
+        '<div class="product-pic" style="background-image: url('+ product.img + ');"></div>'+
+        '<div class="product-info"><div class="product-price">$' + product.price + 
+        '</div><a href="product.html?id=" + product.id + " class="product-button">See More</a></div></div></div>'
+        
+    )
+})
